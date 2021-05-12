dc779650d3a578a6787246e74a94920d2413314d
kaitbr0 <humansare@gmail.com>
2021-05-11 20:20:59 -0400

    hello function

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,17 @@
 package greetings
 
-import "fmt"
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+func Hello(name string) (string, error) {
+	if name == ""{
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
 }
 
 //FunctionName(name paramType) returnType{
@@ -11,3 +19,16 @@ func Hello(name string) string {
 	//equal to var message string
 	//message = fmt.Sprintf('HI %v, blahblah', name)
 //}
+
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail %v! Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main(){
-	message:= greetings.Hello("Lady Gaga")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Lady Gaga")
+	if err != nil{
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
