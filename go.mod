ae0a0458343b312afec276fb70f2e8e82d0d08a7
kaitbr0 <humansare@gmail.com>
2021-05-11 20:03:02 -0400

    imported module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kaitbr0/helloGo.git
 
 go 1.16
+
+require rsc.io/quote v1.5.2
--- hello.go
+++ hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+//declare main package
+import (
+	"fmt"
+
+	"rsc.io/quote"
+) //import fmt package, functions for formatting text
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Println(quote.Opt())
 }
+
+//go mod tidy to import the package
+// go run .
